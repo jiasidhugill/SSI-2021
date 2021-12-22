a88deaf4071728ad03461e2e9e1c58feb1fb0995
Jia Gill <96542484+jiasidhugill@users.noreply.github.com>
2021-12-22 10:42:22 -0800

    Create cleaning-data.py

---
+++ cleaning-data.py
@@ -0,0 +1,5 @@
+# For this project, I needed to combine data from the FEC dataset (campaign finance) and MEDSL datasets (election results). Because of small discrepancies 
+# in how candidates' names were entered into the dataset, I used both the pandas library in python and Google Sheets functionality to combine the datasets.
+# The Google Sheets functionality is difficult to document, but a quick explanation would be that I split the candidates' names into three columns: first name,
+# last name, and middle initial. The rest is done in the following python code.
+# Note: The cleaned datasets used in finding-relationships.py are also uploaded into this repository for ease of access.
